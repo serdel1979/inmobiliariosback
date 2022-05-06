865c542d67c00c6332730247fd42cc28e620cc3f
unknown <sdlbsso@gmail.com>
2022-05-06 12:54:42 -0300

    afuera ormconfig

---
+++ src/migrations/1650902598466-hola.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class hola1650902598466 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
---
+++ src/migrations/1650902693444-segunda.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class segunda1650902693444 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
